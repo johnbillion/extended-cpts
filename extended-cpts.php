93675857b587706edae107aba33cd6c854cd6efc
John Blackbourn <johnbillion@gmail.com>
2014-08-14 17:40:57 +0100

    Add additional rewrite rule tests for the post type archive.

--- extended-cpts.php
+++ extended-cpts.php
@@ -1984,11 +1984,34 @@ class Extended_CPT_Rewrite_Testing extends Extended_Rewrite_Testing {
 
 		global $wp_rewrite;
 
-		$struct = $wp_rewrite->extra_permastructs[$this->cpt->post_type];
-		$name   = get_post_type_object( $this->cpt->post_type )->labels->name;
+		$struct     = $wp_rewrite->extra_permastructs[$this->cpt->post_type];
+		$pto        = get_post_type_object( $this->cpt->post_type );
+		$additional = array();
+
+		// Post type archive rewrites are generated separately. See the `has_archive` handling in `register_post_type()`.
+		if ( $pto->has_archive ) {
+			$archive_slug = $pto->has_archive === true ? $pto->rewrite['slug'] : $pto->has_archive;
+
+			if ( $pto->rewrite['with_front'] ) {
+				$archive_slug = substr( $wp_rewrite->front, 1 ) . $archive_slug;
+			} else {
+				$archive_slug = $wp_rewrite->root . $archive_slug;
+			}
+
+			$additional["{$archive_slug}/?$"] = "index.php?post_type={$this->cpt->post_type}";
+
+			if ( $pto->rewrite['feeds'] && $wp_rewrite->feeds ) {
+				$feeds = '(' . trim( implode( '|', $wp_rewrite->feeds ) ) . ')';
+				$additional["{$archive_slug}/feed/{$feeds}/?$"] = "index.php?post_type={$this->cpt->post_type}" . '&feed=$matches[1]';
+				$additional["{$archive_slug}/{$feeds}/?$"] = "index.php?post_type={$this->cpt->post_type}" . '&feed=$matches[1]';
+			}
+			if ( $pto->rewrite['pages'] ) {
+				$additional["{$archive_slug}/{$wp_rewrite->pagination_base}/([0-9]{1,})/?$"] = "index.php?post_type={$this->cpt->post_type}" . '&paged=$matches[1]';
+			}
+		}
 
 		return array(
-			$name => $this->get_rewrites( $struct ),
+			$pto->labels->name => $this->get_rewrites( $struct, $additional ),
 		);
 
 	}
