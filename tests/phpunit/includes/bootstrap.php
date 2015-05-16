a229d1f14fcaba0b48b6038dd7cca779525c00d8
John Blackbourn <johnbillion@gmail.com>
2015-05-16 03:34:02 +0100

    Move phpunit files into a subdirectory.

--- tests/phpunit/includes/bootstrap.php
+++ tests/phpunit/includes/bootstrap.php
@@ -6,9 +6,9 @@ if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../extended-cpts.php';
+	require dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/extended-cpts.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
-require dirname( __FILE__ ) . '/extended-cpts-test.php';
+require dirname( dirname( __FILE__ ) ) . '/extended-cpts-test.php';
