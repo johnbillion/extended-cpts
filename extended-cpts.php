f8ae985a433f80f41fd89f679ad48c050a45220e
John Blackbourn <johnbillion@gmail.com>
2015-12-11 17:52:58 +0000

    ExtendedCPTs is no longer a plugin :o

--- extended-cpts.php
+++ extended-cpts.php
@@ -1,28 +1,24 @@
 <?php
-/*
-Plugin Name:  Extended CPTs
-Description:  Extended custom post types.
-Version:      2.5.2
-Plugin URI:   https://github.com/johnbillion/extended-cpts
-Author:       John Blackbourn
-Author URI:   https://johnblackbourn.com
-Text Domain:  extended-cpts
-Domain Path:  /languages/
-License:      GPL v2 or later
-
-Copyright © 2012-2015 John Blackbourn
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-*/
+/**
+ * Extended custom post types for WordPress.
+ *
+ * @package   ExtendedCPTs
+ * @version   3.0.0
+ * @author    John Blackbourn <https://johnblackbourn.com>
+ * @link      https://github.com/johnbillion/extended-cpts
+ * @copyright 2012-2015 John Blackbourn
+ * @license   GPL v2 or later
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
 if ( ! function_exists( 'register_extended_post_type' ) ) {
 /**
