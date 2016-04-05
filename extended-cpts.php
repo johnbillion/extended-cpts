c72d09115261b42b64d7e5665054eeea928197d6
Evan Mattson <aaemnnosttv@users.noreply.github.com>
2016-04-05 16:00:52 -0400

    add Extended_CPT::init() method for loading

--- extended-cpts.php
+++ extended-cpts.php
@@ -781,6 +781,15 @@ class Extended_CPT {
 		return get_taxonomy( $taxonomy );
 
 	}
+	
+	/**
+	 * Initialize the library and load global functions
+	 */ 
+	public static function init() {
+		
+		// load the library
+		
+	}
 
 }
 }
