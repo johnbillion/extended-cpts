b7eaff5372b4fc53dc8c1d7d55b766f8f8add5da
John Blackbourn <johnbillion@gmail.com>
2017-03-21 17:38:31 +0000

    Update the copyright year.

--- extended-cpts.php
+++ extended-cpts.php
@@ -6,7 +6,7 @@
  * @version   3.0.3
  * @author    John Blackbourn <https://johnblackbourn.com>
  * @link      https://github.com/johnbillion/extended-cpts
- * @copyright 2012-2016 John Blackbourn
+ * @copyright 2012-2017 John Blackbourn
  * @license   GPL v2 or later
  *
  * This program is free software; you can redistribute it and/or modify
