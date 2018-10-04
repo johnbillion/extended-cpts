7110de2b959d218709a7e993706d53867b1663ec
John Blackbourn <johnbillion@gmail.com>
2018-10-04 15:51:23 +0100

    Version 4.2.1.

--- extended-cpts.php
+++ extended-cpts.php
@@ -7,9 +7,9 @@ declare( strict_types=1 );
  * @package   ExtendedCPTs
  * @author    John Blackbourn <https://johnblackbourn.com>
  * @link      https://github.com/johnbillion/extended-cpts
- * @copyright 2012-2017 John Blackbourn
+ * @copyright 2012-2018 John Blackbourn
  * @license   GPL v2 or later
- * @version   4.2.0
+ * @version   4.2.1
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
