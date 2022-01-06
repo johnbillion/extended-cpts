2058169e5fb8f00905727d4ca3cb67b7a7312b95
John Blackbourn <john@humanmade.com>
2022-01-06 13:54:14 +0100

    I can live without this.

--- tests/integration/SetupTest.php
+++ tests/integration/SetupTest.php
@@ -52,19 +52,6 @@ class Setup extends Test {
 		$this->assertSame( $min, $matches['version'] );
 	}
 
-	public function testVersionMatchesTag() {
-		$filename = dirname( dirname( dirname( __FILE__ ) ) ) . '/extended-cpts.php';
-		$this->assertFileExists( $filename );
-
-		$file    = file_get_contents( $filename );
-		$pattern = '/@version\s+(?P<version>[0-9]\.[0-9]\.[0-9])$/m';
-		$result  = preg_match( $pattern, $file, $matches );
-		$this->assertNotEmpty( $result );
-
-		$tag = trim( exec( 'git describe --abbrev=0 --tags' ) );
-		$this->assertSame( $tag, $matches['version'] );
-	}
-
 	public function testPostTypeArgsAreCorrect() {
 
 		$this->assertEquals( 'hello',  $this->cpts['hello']->post_type );
