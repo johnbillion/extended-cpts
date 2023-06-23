507782d04b6a9cf58ffa9e697edb58e84562dc6a
John Blackbourn <john@humanmade.com>
2023-06-23 22:50:24 +0200

    Make this check more explicit.

--- src/PostTypeAdmin.php
+++ src/PostTypeAdmin.php
@@ -708,12 +708,11 @@ ICONCSS;
 	public function post_updated_messages( array $messages ): array {
 		global $post;
 
-		/** @var \WP_Post_Type */
 		$pto = get_post_type_object( $this->cpt->post_type );
-		
-		if (is_null($pto)) {
-            return $messages;
-        }
+
+		if ( ! ( $pto instanceof \WP_Post_Type ) ) {
+			return $messages;
+		}
 
 		$messages[ $this->cpt->post_type ] = [
 			1 => sprintf(
